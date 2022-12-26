dda7eb9341a7654c3897f6bc5fb2658521720367
Luan Kobs <luankkobs@gmail.com>
2022-12-26 15:24:54 -0300

    Adding logs

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"github.com/luankkobs/go-crud/src/configuration/logger"
 	"github.com/luankkobs/go-crud/src/controller/routes"
 	"log"
 )
 
 func main() {
+	logger.Info("Starting application")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
--- src/controller/create_user.go
+++ src/controller/create_user.go
@@ -1,24 +1,43 @@
 package controller
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/luankkobs/go-crud/src/configuration/logger"
 	"github.com/luankkobs/go-crud/src/configuration/validation"
 	"github.com/luankkobs/go-crud/src/controller/model/request"
-	"log"
+	"github.com/luankkobs/go-crud/src/controller/model/response"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("Error trying to bind user request: %s", err.Error())
+		logger.Error("Error trying to validate user info", err,
+			zap.String("journey", "createUser"))
 		errRest := validation.ValidateUserError(err)
+
 		c.JSON(errRest.Code, errRest)
 		return
 	}
 
-	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Email: userRequest.Email,
+		Name:  userRequest.Name,
+		Age:   userRequest.Age,
+	}
 
+	logger.Info("User created successfully",
+		zapcore.Field{
+			Key:    "journey",
+			String: "createUser",
+		})
+
+	c.JSON(http.StatusOK, response)
 }
